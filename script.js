0a0d042337b0e24cdac228c52c172b361ff54cd1
Sean McPherson <git@seanmcp.com>
2024-01-04 09:22:31 -0500

    working with different digits, number of addends

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ class Int {
     }
 
     column(index) {
-        return this.string[(this.string.length - 1) - index];
+        return this.string[this.string.length - 1 - index];
     }
 
     get length() {
@@ -17,8 +17,8 @@ class Int {
         const place = this.string.slice(-1);
         return {
             carry: carry ? parseInt(carry) : null,
-            place: place ? parseInt(place) : null
-        }
+            place: place ? parseInt(place) : null,
+        };
     }
 }
 
@@ -26,48 +26,77 @@ const DEBUG_MODE = true;
 const log = DEBUG_MODE ? console.log : () => { };
 
 const app = document.querySelector("#app");
-const numbers = [425, 76];
-const ints = numbers.map(number => new Int(number)).toSorted((a, b) => b.length - a.length);
+const numbers = [33, 33, 34];
+const ints = numbers
+    .map((number) => new Int(number))
+    .toSorted((a, b) => b.length - a.length);
 const max = ints[0].length;
 
 const carrySums = [null];
 const placeSums = [];
 
-let carry
+let carry;
 
 for (let i = 0; i < max; i++) {
-    log("column", i)
+    log("column", i);
     let sum = ints.reduce((acc, cur) => {
         const column = cur.column(i);
-        log("\taddend", column)
+        log("\taddend", column);
         return acc + (column ? parseInt(column) : 0);
     }, 0);
     if (carry) {
-        log("\tcarry", carry)
+        log("\tcarry", carry);
         sum += carry;
         carry = undefined;
     }
-    log("\tsum:", sum)
+    log("\tsum:", sum);
     const int = new Int(sum);
-    carry = parseInt(int.sum.carry);
+    carry = int.sum.carry;
 
     placeSums.unshift(int.sum.place);
     carrySums.unshift(int.sum.carry);
 }
 
-console.log({
+if (carry) {
+    // For a hanging carry, remove first carry sum and add it to place sums
+    placeSums.unshift(carrySums.shift());
+    carry = undefined;
+}
+
+log({
     carrySums,
-    placeSums
-})
+    placeSums,
+});
+
+function getInputMarkup(sum) {
+    return `<input inputmode="numeric" max="9" min="0" pattern="^${sum}$" type="text" required />`;
+}
 
 app.innerHTML = `
     <div id="carry-sums">
-        ${carrySums.reduce((html, sum) => html + (sum != undefined ? `<input pattern="^${sum}$" />` : "<span></span>"), "")}
+        ${carrySums.reduce(
+    (html, sum) =>
+        html + (sum != undefined ? getInputMarkup(sum) : "<span></span>"),
+    ""
+)}
     </div>
     <div id="addends">
-        ${ints.reduce((html, int) => html + "<div class='addend'>" + int.string.split("").reduce((str, char) => str + `<span>${char}</span>`, "") + "</div>", "")}
+        ${ints.reduce(
+    (html, int) =>
+        html +
+        "<div class='addend'>" +
+        int.string
+            .split("")
+            .reduce((str, char) => str + `<span>${char}</span>`, "") +
+        "</div>",
+    ""
+)}
     </div>
     <div id="place-sums">
-        ${placeSums.reduce((html, sum) => html + (sum != undefined ? `<input pattern="^${sum}$" />` : "<span></span>"), "")}
+        ${placeSums.reduce(
+    (html, sum) =>
+        html + (sum != undefined ? getInputMarkup(sum) : "<span></span>"),
+    ""
+)}
     </div>
-`
+`;
